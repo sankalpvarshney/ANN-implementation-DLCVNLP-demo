7113b038a12bf60fcf7c4012040766eb537d3dbd
SANKALP VARSHNEY <sankalp.varshney@biocube.ai>
2022-10-05 11:58:35 +0530

    Update setup file

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.2",
-    author="c17hawke",
+    author="svarshney",
     description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/ANN-implementation-DLCVNLP-demo",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/sankalpvarshney/ANN-implementation-DLCVNLP-demo",
+    author_email="sankalpvarshney24@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
